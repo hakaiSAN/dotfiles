a4cc5b37a31d72da6ee7b47983f28ac6f787eac0
hakaiSAN <kawai.arch.jk@gmail.com>
2019-06-03 16:35:21 +0900

    bug fix shell added

---
+++ vimproc_bugfix.sh
@@ -0,0 +1,3 @@
+cd ~/.cache/dein/repos/github.com/Shougo/vimproc.vim/lib
+make
+ln -sf ~/.cache/dein/repos/github.com/Shougo/vimproc.vim/lib/vimproc_linux64.so ~/.cache/dein/.cache/.vimrc/.dein/lib/vimproc_linux64.so
